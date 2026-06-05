14a465b111c445ba5fc5402b1c56f49d5673a56e
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-06-05 23:24:35 +0530

    changes

--- routes/performance.py
+++ routes/performance.py
@@ -54,11 +54,3 @@ def edit(rev_id):
     return redirect(url_for('performance.index'))
 
 
-@performance_bp.route('/delete/<int:rev_id>', methods=['POST'])
-@login_required
-def delete(rev_id):
-    rev = PerformanceReview.query.get_or_404(rev_id)
-    db.session.delete(rev)
-    db.session.commit()
-    flash('Review deleted.', 'success')
-    return redirect(url_for('performance.index'))
